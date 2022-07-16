236e583b311af97076811a7479b54a85abdb531d
Kevin Coffey <kevdougful@gmail.com>
2022-07-16 14:45:24 -0500

    Actually Add Logging

--- enrollment.tf
+++ enrollment.tf
@@ -1,6 +1,6 @@
 module "enrollment-backend" {
   source  = "app.terraform.io/abcballpark/enrollment-backend/aws"
-  version = "0.1.18"
+  version = "0.1.20"
 
   api_name      = "enrollment-api"
   user_pool_arn = module.auth_backend.pool_arn
