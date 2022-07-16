0ec4307d2cbd0682e306a551fd69dda18c83b887
Kevin Coffey <kevdougful@gmail.com>
2022-07-15 22:16:08 -0500

    Fix Format

--- enrollment.tf
+++ enrollment.tf
@@ -1,6 +1,6 @@
 module "enrollment-backend" {
   source  = "app.terraform.io/abcballpark/enrollment-backend/aws"
-  version = "0.1.11"
+  version = "0.1.13"
 
   api_name      = "enrollment-api"
   user_pool_arn = module.auth_backend.pool_arn
