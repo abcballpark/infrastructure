9c7183ba7a01a0a2109d36e0e8f42b9101998c9e
Kevin Coffey <kevdougful@gmail.com>
2022-07-22 14:14:38 -0500

    Add Participant Resource

--- enrollment.tf
+++ enrollment.tf
@@ -1,6 +1,6 @@
 module "enrollment-backend" {
   source  = "app.terraform.io/abcballpark/enrollment-backend/aws"
-  version = "0.1.21"
+  version = "0.1.22"
 
   api_name      = "enrollment-api"
   user_pool_arn = module.auth_backend.pool_arn
