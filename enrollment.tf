d690a057188497cb0eadaf645f20134dc888b466
Kevin Coffey <kevdougful@gmail.com>
2022-07-22 17:03:17 -0500

    Fix Table Linkage

--- enrollment.tf
+++ enrollment.tf
@@ -1,6 +1,6 @@
 module "enrollment-backend" {
   source  = "app.terraform.io/abcballpark/enrollment-backend/aws"
-  version = "0.1.26"
+  version = "0.1.31"
 
   api_name      = "enrollment-api"
   user_pool_arn = module.auth_backend.pool_arn
