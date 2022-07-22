2085443bd93866017f44dc03919d0e30a7e644f7
Kevin Coffey <kevdougful@gmail.com>
2022-07-22 14:28:22 -0500

    Fix Naming Conflicts

--- auth.tf
+++ auth.tf
@@ -1,6 +1,6 @@
 module "auth_backend" {
   source  = "app.terraform.io/abcballpark/auth-backend/aws"
-  version = "0.1.10"
+  version = "~> 0.1.10"
 
   name = "main"
 }
--- enrollment.tf
+++ enrollment.tf
@@ -1,6 +1,6 @@
 module "enrollment-backend" {
   source  = "app.terraform.io/abcballpark/enrollment-backend/aws"
-  version = "0.1.22"
+  version = "~> 0.1.23"
 
   api_name      = "enrollment-api"
   user_pool_arn = module.auth_backend.pool_arn
